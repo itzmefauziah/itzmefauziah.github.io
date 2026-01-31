ab4d5df1d5b0b7a0a4a571eac245974b19ed353b
Nurul Fauziah <164123789+itzmefauziah@users.noreply.github.com>
2026-01-31 15:55:55 +0800

    Update slider.js

--- slider.js
+++ slider.js
@@ -16,8 +16,4 @@ var swiper = new Swiper(".mySwiper", {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
   },
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
-  },
 });
