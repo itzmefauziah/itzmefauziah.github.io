cc0f1f3db753d133c0da31c1f65c5c843b754391
Nurul Fauziah <164123789+itzmefauziah@users.noreply.github.com>
2026-01-31 15:54:27 +0800

    Update and rename slide.js to slider.js

---
+++ slider.js
@@ -0,0 +1,23 @@
+// slider.js (NEW)
+var swiper = new Swiper(".mySwiper", {
+  effect: "coverflow",
+  grabCursor: true,
+  centeredSlides: true,
+  slidesPerView: "auto",
+  loop: true,
+  coverflowEffect: {
+    rotate: 15,
+    stretch: 0,
+    depth: 50,
+    modifier: 1,
+    slideShadows: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
