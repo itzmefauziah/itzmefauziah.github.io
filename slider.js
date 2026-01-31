af1fabf47a830b0456be8e46c43a467f930cef30
Nurul Fauziah <164123789+itzmefauziah@users.noreply.github.com>
2026-01-31 17:06:13 +0800

    Update slider.js

--- slider.js
+++ slider.js
@@ -11,9 +11,14 @@ var swiper = new Swiper(".mySwiper", {
     modifier: 1,
     slideShadows: true,
   },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
   breakpoints: {
     320: { slidesPerView: 1, spaceBetween: 10 },
     640: { slidesPerView: 2, spaceBetween: 20 },
     1024: { slidesPerView: 3, spaceBetween: 30 },
   },
 });
+
