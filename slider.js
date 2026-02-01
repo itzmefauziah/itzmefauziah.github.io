2451eb4e0fe253787b976c645fd7065d38a113fe
Nurul Fauziah <164123789+itzmefauziah@users.noreply.github.com>
2026-02-01 18:31:31 +0800

    Update slider.js

--- slider.js
+++ slider.js
@@ -11,6 +11,10 @@ var swiper = new Swiper(".mySwiper", {
     modifier: 1,
     slideShadows: true,
   },
+    navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
   breakpoints: {
     320: { slidesPerView: 1, spaceBetween: 10 },
     640: { slidesPerView: 2, spaceBetween: 20 },
